b47818baaa575b0f756e5f10ef0104438c407d6c
udayhasan <34761590+udayhasan@users.noreply.github.com>
2018-04-01 06:46:15 +0600

    Update compiler_RISCI_16bit.py

--- compiler_RISCI_16bit.py
+++ compiler_RISCI_16bit.py
@@ -1,11 +1,12 @@
 import os
+os.system("clear")
 
 off_jump="012b"
 off_ldstbr="06b"
 off_other="03b"
 
 ldstbr_list=["lw", "sw", "beq", "bne"]
-other_list=["add", "sub", "inv", "invert", "lsl", "lsr", "and", "or", "slt", "xor"]
+other_list=["add", "sub", "inv", "invert", "lsl", "lsr", "and", "or", "slt", "xor", "mov"]
 
 def instruction(ins):
     code=""
@@ -29,17 +30,17 @@ def instruction(ins):
         code="1000"
     elif(ins=="slt"):
         code="1001"
-    elif(ins=="xor"):
+    elif(ins=="ham"):
         code="1010"
-    elif(ins=="mov"):
+    elif(ins=="beq"):
         code="1011"
-    elif(ins=="addi"):
+    elif(ins=="bne"):
         code="1100"
-    elif(ins=="subi"):
+    elif(ins=="j"):
         code="1101"
-    elif(ins=="srl"):
+    elif(ins=="xor"):
         code="1110"
-    elif(ins=="sra"):
+    elif(ins=="mov"):
         code="1111"
     else:
         code="error"
@@ -54,7 +55,7 @@ def address(add):
     elif(add=="r1"):
         code="001"
     elif(add=="r2"):
-        code=="010"
+        code="010"
     elif(add=="r3"):
         code="011"
     else:
@@ -66,6 +67,7 @@ def address(add):
 def offset(off, ins):
     code=""
     val=int(off)
+
     if(ins=="j"):
         code=format(val, off_jump)
     elif(ins in ldstbr_list):
@@ -80,53 +82,81 @@ def offset(off, ins):
 
 def jump_func(data):
 	code=""
-	code=instruction(data[0])+offset(data[1], data[0])
+	code+=instruction(data[0])
+	code+=offset(data[1], data[0])
 	print code
 	return code
 
 def ldstbr_func(data):
 	code=""
-	code=instruction(data[0])+address(data[2])+address(data[1])+offset(data[3], data[0])
+	code+=instruction(data[0])
+	code+=address(data[2])
+	code+=address(data[1])
+	code+=offset(data[3], data[0])
 	print code
 	return code
 
 def data_processing_func(data):
 	code=""
-	code=instruction(data[0])+address(data[2])+address(data[3])+address(data[1])+offset(0, data[0])
+	code+=instruction(data[0])
+	code+=address(data[2])
+	code+=address(data[3])
+	code+=address(data[1])
+	code+=offset(0, data[0])
 	print code
 	return code
 
+def read_file():
+	    fname=raw_input("Name of instruction file: ")
+	    fname_new=fname.split(".")
+	    fname2=fname_new[0]+".bin"
+	    
+	    f1=open(fname, "r")
+	    rd=f1.read()
+	    rd=rd.lower()
+	    f1.close()
+	    dt=rd.split("\n")
+	    dt.pop(len(dt)-1)
+
+	    for line in dt:
+	        data=""
+	        put=""
+	        ins=""
+	        data=line.split(" ")
+
+	        if(data[0]=="j"):
+	        	put=jump_func(data)
+	        elif(data[0] in ldstbr_list):
+	        	put=ldstbr_func(data)
+	        else:
+	        	put=data_processing_func(data)
+
+	        f2=open(fname2, "a")
+	        f2.write(put)
+	        f2.write("\n")
+	        f2.close
+
+def make_file():
+	end=0
+	put=""
+	ins=raw_input("Instruction: ")
+	put+=ins+" "
+	if(ins=="j"):
+		put+=raw_input("Offset: ")
+	elif (ins in ldstbr):
+		put+=raw_input("rs1: ")
+		put+=raw_input("rs2: ")
+		put+=raw_input("Offset: ")
+
+
 if __name__=="__main__":
-    fname=raw_input("Name of instruction file: ")
-    fname_new=fname.split(".")
-    fname2=fname_new[0]+".bin"
+	select=input("1. Make instruction file then compile\n2. Read Instruction from a file\nPlease, select: ")
+    if(select==1):
+    	make_file()
+    else:
+    	read_file()
+
     
-    f1=open(fname, "r")
-    rd=f1.read()
-    rd=rd.lower()
-    f1.close()
-    dt=rd.split("\n")
-    dt.pop(len(dt)-1)
-
-    for line in dt:
-        data=""
-        put=""
-        ins=""
-        data=line.split(" ")
-        print data
-        ins=instruction(data[0])
-
-        if(ins=="jump"):
-        	put=jump_func(data)
-        elif(ins in ldstbr_list):
-        	put=ldstbr_func(data)
-        else:
-        	put=data_processing_func(data)
-
-        f2=open(fname2, "a")
-        f2.write(put)
-        f2.write("\n")
-        f2.close
 
 
         
